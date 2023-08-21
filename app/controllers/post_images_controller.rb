9fea372f8d8343facda46572eab5ebf0574eada0
kitamura-ryusei <141385505+kitamura-ryusei@users.noreply.github.com>
2023-08-21 14:16:14 +0000

    ２２章完了

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,12 +6,15 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
@@ -28,7 +31,7 @@ class PostImagesController < ApplicationController
   private
   
   def post_image_params
-    params.require(:post_image).permit(:shop_name, :iamge, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
   
 end
